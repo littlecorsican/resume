9a7e11c72a297f1f03c2a54efafb970ef7338cd2
gnomemage1990@gmail.com <gnomemage1990@gmail.com>
2024-10-06 12:55:20 +0800

    Update skills.jsx

--- react/resume/src/components/skills.jsx
+++ react/resume/src/components/skills.jsx
@@ -8,7 +8,7 @@ function Skills() {
   return (
     <div className="py-4 px-10 text-3xl">
         <p>Javascript, PHP, Python, C, Nodejs, Nextjs, React, OpenShift, Linux, AWS, Typescript, Tailwind, HTML5, CSS3, Flask,
-        FastAPI, MSSQL, MySQL, PostgresSQL, Datadog, Jaeger, Ansible, Prometheus, Grafana</p>
+        FastAPI, MSSQL, MySQL, PostgresSQL, Datadog, Jaeger, Ansible, Prometheus, Grafana, Terraform</p>
     </div>
   );
 }
